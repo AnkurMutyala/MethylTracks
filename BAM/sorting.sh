7c8308051f93fba8bf0213568c6fc7902791ccc3
AnkurMutyala <ankurmut@unc.edu>
2026-09-08 00:31:11 -0400

    Create sorting.sh

---
+++ BAM/sorting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=sorting
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -e [path/to/error/folder/].%j.err
+#SBATCH --time=24:00:00
+
+#Load Samtools
+
+module load samtools
+
+#Use Samtools sort command
+
+samtools sort -o </desired/path/and/name/of/sorted/BAM> -o </path/to/input/unsorted/BAM>
