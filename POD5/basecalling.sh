b0b675d1fed7cd63c49c262310a446b94292aa5e
AnkurMutyala <ankurmut@unc.edu>
2026-09-09 01:38:09 -0400

    Update basecalling.sh

--- POD5/basecalling.sh
+++ POD5/basecalling.sh
@@ -9,20 +9,21 @@
 
 #Load Dorado version
 
-module load dorado/2.1.0
+module load dorado/2.1.0 \
 
 #Set working directory
 
-cd <path/to/directory/>
+cd <path/to/working/directory/> \
 
 #Download basecalling model with desired specifications: 
 #--model {ANALYTE}_{PORE-TYPE}_{KIT-CHEMISTRY}_{TRANSLOCATION-SPEED}_{ACCURACY@SIMPLEX MODEL}_{MOD@MOD-MODEL}
 
-dorado download --model dna_r10.4.1_e8.2_400bps_hac@v6.0.0_6mA@v1
+dorado download --model dna_r10.4.1_e8.2_400bps_hac@v6.0.0_6mA@v1 \
 
 #Run basecalling
 
-    dorado basecaller dna_r10.4.1_e8.2_400bps_hac@v6.0.0 </PATH/TO/INPUT/POD5> \
-    --modified-bases-models dna_r10.4.1_e8.2_400bps_hac@v6.0.0_6mA@v1 \
-    --trim --reference /example/directory/hg38.fa \
-    -o </DESIRED/PATH/AND/NAME/OF/OUTPUT/BAM>
+dorado basecaller dna_r10.4.1_e8.2_400bps_hac@v6.0.0 </path/to/input/POD5> \
+--modified-bases-models dna_r10.4.1_e8.2_400bps_hac@v6.0.0_6mA@v1 \
+--trim \
+--reference </path/to/reference/genome/hg38.fa> \
+-o </desired/path/and/name/of/output/BAM>
